f5a7b4d8aaf2a64507ca4bf0cb87e64f57416edd
dltmthilina <dltmthilinamduranga@gmail.com>
2023-05-15 19:11:16 +0530

    generate and download pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
-use PDF;
+use Dompdf\Dompdf;
+
+
 
 class PDFController extends Controller
 {
     
     public function download(){
 
-        $data = [
-            'title' => 'Welcome to ItSolutionStuff.com',
-            'date' => date('m/d/Y')
-        ];
-        $pdf = PDF::view('myPDF', $data);
+     try{
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml('<h1>Hello, World!</h1>');
+        $dompdf->setPaper('A4', 'landscape');
+        $dompdf->render();
+        $dompdf->stream();
 
-        return $pdf->download('itsolutionstuff.pdf');
+        }catch(Exception $e){
+            Log::error($e->getMessage());
+            
+            // Return an error response
+            return response()->json(['error' => 'PDF generation failed'], 500);
+        }
     }
 }
