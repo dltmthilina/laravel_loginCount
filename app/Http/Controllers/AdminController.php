843a888b3535564aafdf80c598538df5e06ec8cd
dltmthilina <dltmthilinamduranga@gmail.com>
2023-05-16 15:08:33 +0530

    create saveUser function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,10 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
    public function createUser(){
         return view('userregister');
+   }
+
+   public function storeUser(Request $request){
+        $formData = new User();
+        $formData->name = $request->input('name');
+        $formData->email = $request->input('nic');
+        $formData->message = $request->input('message');
+        $formData->save();
    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nic')->unique();
             $table->string('password');
+            $table->string('contact');
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
